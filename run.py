1d625573d52b145adb0ef0767b279e02ef2faa03
Guanzhuo Qiao <gqiao@stevens.edu>
2020-03-26 22:07:48 -0400

    little modification

--- run.py
+++ run.py
@@ -1,271 +1,12 @@
 import sys
 import time
-
+import numpy as np
+import pandas as pd
 import shift
+import datetime
 
-
-def demo_01(trader: shift.Trader):
-    """
-    This method submits a limit buy order by indicating order type, symbol, size, and limit price.
-    :param trader:
-    :return:
-    """
-
-    limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "AAPL", 1, 10.00)
-    trader.submit_order(limit_buy)
-
-    return
-
-
-def demo_02(trader: shift.Trader):
-    """
-    This method submits 2 limit buy orders by indicating order type, symbol, size, and limit price.
-    :param trader:
-    :return:
-    """
-
-    aapl_limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "AAPL", 10, 10.00)
-    trader.submit_order(aapl_limit_buy)
-
-    xom_limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, "XOM", 10, 10.00)
-    trader.submit_order(xom_limit_buy)
-
-    return
-
-
-def demo_03(trader: shift.Trader):
-    """
-    This method prints the local bid order book for corresponding symbols.
-    :param trader:
-    :return:
-    """
-
-    print("AAPL:")
-    print("  Price\t\tSize\t  Dest\t\tTime")
-    for order in trader.get_order_book("AAPL", shift.OrderBookType.LOCAL_BID):
-        print(
-            "%7.2f\t\t%4d\t%6s\t\t%19s"
-            % (order.price, order.size, order.destination, order.time)
-        )
-
-    print()
-
-    print("XOM:")
-    print("  Price\t\tSize\t  Dest\t\tTime")
-    for order in trader.get_order_book("XOM", shift.OrderBookType.LOCAL_BID):
-        print(
-            "%7.2f\t\t%4d\t%6s\t\t%19s"
-            % (order.price, order.size, order.destination, order.time)
-        )
-
-
-def demo_04(trader: shift.Trader):
-    """
-    This method prints all current waiting orders information.
-    :param trader:
-    :return:
-    """
-
-    print(
-        "Symbol\t\t\t\tType\t  Price\t\tSize\tExecuted\tID\t\t\t\t\t\t\t\t\t\t\t\t\t\t Status\t\tTimestamp"
-    )
-    for order in trader.get_waiting_list():
-        print(
-            "%6s\t%16s\t%7.2f\t\t%4d\t\t%4d\t%36s\t%23s\t\t%26s"
-            % (
-                order.symbol,
-                order.type,
-                order.price,
-                order.size,
-                order.executed_size,
-                order.id,
-                order.status,
-                order.timestamp,
-            )
-        )
-
-    return
-
-
-def demo_05(trader: shift.Trader):
-    """
-    This method cancels all the orders in the waiting list.
-    :param trader:
-    :return:
-    """
-
-    print(
-        "Symbol\t\t\t\tType\t  Price\t\tSize\tExecuted\tID\t\t\t\t\t\t\t\t\t\t\t\t\t\t Status\t\tTimestamp"
-    )
-    for order in trader.get_waiting_list():
-        print(
-            "%6s\t%16s\t%7.2f\t\t%4d\t\t%4d\t%36s\t%23s\t\t%26s"
-            % (
-                order.symbol,
-                order.type,
-                order.price,
-                order.size,
-                order.executed_size,
-                order.id,
-                order.status,
-                order.timestamp,
-            )
-        )
-
-    print()
-
-    print("Waiting list size: " + str(trader.get_waiting_list_size()))
-
-    print("Canceling all pending orders...", end=" ")
-
-    # trader.cancel_all_pending_orders() also works
-    for order in trader.get_waiting_list():
-        trader.submit_cancellation(order)
-
-    i = 0
-    while trader.get_waiting_list_size() > 0:
-        i += 1
-        print(i, end=" ")
-        time.sleep(1)
-
-    print()
-
-    print("Waiting list size: " + str(trader.get_waiting_list_size()))
-
-    return
-
-
-def demo_06(trader: shift.Trader):
-    """
-    This method shows how to submit market buy orders.
-    :param trader:
-    :return:
-    """
-
-    aapl_market_buy = shift.Order(shift.Order.Type.MARKET_BUY, "AAPL", 1)
-    trader.submit_order(aapl_market_buy)
-
-    xom_market_buy = shift.Order(shift.Order.Type.MARKET_BUY, "XOM", 1)
-    trader.submit_order(xom_market_buy)
-
-    return
-
-
-def demo_07(trader: shift.Trader):
-    """
-    This method provides information on the structure of PortfolioSummary and PortfolioItem objects:
-     get_portfolio_summary() returns a PortfolioSummary object with the following data:
-     1. Total Buying Power (get_total_bp())
-     2. Total Shares (get_total_shares())
-     3. Total Realized Profit/Loss (get_total_realized_pl())
-     4. Timestamp of Last Update (get_timestamp())
-
-     get_portfolio_items() returns a dictionary with "symbol" as keys and PortfolioItem as values,
-     with each providing the following information:
-     1. Symbol (get_symbol())
-     2. Shares (get_shares())
-     3. Price (get_price())
-     4. Realized Profit/Loss (get_realized_pl())
-     5. Timestamp of Last Update (get_timestamp())
-    :param trader:
-    :return:
-    """
-
-    print("Buying Power\tTotal Shares\tTotal P&L\tTimestamp")
-    print(
-        "%12.2f\t%12d\t%9.2f\t%26s"
-        % (
-            trader.get_portfolio_summary().get_total_bp(),
-            trader.get_portfolio_summary().get_total_shares(),
-            trader.get_portfolio_summary().get_total_realized_pl(),
-            trader.get_portfolio_summary().get_timestamp(),
-        )
-    )
-
-    print()
-
-    print("Symbol\t\tShares\t\tPrice\t\t  P&L\tTimestamp")
-    for item in trader.get_portfolio_items().values():
-        print(
-            "%6s\t\t%6d\t%9.2f\t%9.2f\t%26s"
-            % (
-                item.get_symbol(),
-                item.get_shares(),
-                item.get_price(),
-                item.get_realized_pl(),
-                item.get_timestamp(),
-            )
-        )
-
-    return
-
-
-def demo_08(trader: shift.Trader):
-    """
-    This method shows how to submit market sell orders.
-    :param trader:
-    :return:
-    """
-
-    aapl_market_sell = shift.Order(shift.Order.Type.MARKET_SELL, "AAPL", 1)
-    trader.submit_order(aapl_market_sell)
-
-    xom_market_sell = shift.Order(shift.Order.Type.MARKET_SELL, "XOM", 1)
-    trader.submit_order(xom_market_sell)
-
-    return
-
-
-def demo_09(trader: shift.Trader):
-    """
-    This method prints all submitted orders information.
-    :param trader:
-    :return:
-    """
-
-    print(
-        "Symbol\t\t\t\tType\t  Price\t\tSize\tExecuted\tID\t\t\t\t\t\t\t\t\t\t\t\t\t\t Status\t\tTimestamp"
-    )
-    for order in trader.get_submitted_orders():
-        if order.status == shift.Order.Status.FILLED:
-            price = order.executed_price
-        else:
-            price = order.price
-        print(
-            "%6s\t%16s\t%7.2f\t\t%4d\t\t%4d\t%36s\t%23s\t\t%26s"
-            % (
-                order.symbol,
-                order.type,
-                price,
-                order.size,
-                order.executed_size,
-                order.id,
-                order.status,
-                order.timestamp,
-            )
-        )
-
-    return
-
-
-def demo_10(trader: shift.Trader):
-    """
-    This method prints the global bid order book for a corresponding symbol and type.
-    :param trader:
-    :return:
-    """
-
-    print("  Price\t\tSize\t  Dest\t\tTime")
-    for order in trader.get_order_book("AAPL", shift.OrderBookType.GLOBAL_BID, 5):
-        print(
-            "%7.2f\t\t%4d\t%6s\t\t%19s"
-            % (order.price, order.size, order.destination, order.time)
-        )
-
-
-def main(argv):
-    # create trader object
-    trader = shift.Trader("test001")
+def subscribe():
+    trader = shift.Trader("test002")
 
     # connect and subscribe to all available order books
     try:
@@ -275,24 +16,92 @@ def main(argv):
         print(e)
     except shift.ConnectionTimeoutError as e:
         print(e)
+    return trader
 
-    for _ in range(10):
-        # demo_01(trader)
-        # demo_02(trader)
-        # demo_03(trader)
-        # demo_04(trader)
-        # demo_05(trader)
-        demo_06(trader)
-        # demo_07(trader)
-        # demo_08(trader)
-        # demo_09(trader)
-        # demo_10(trader)
-        time.sleep(600)
-    # disconnect
-    trader.disconnect()
+def market_order(trader: shift.Trader, order_type, symbol, contract_size):
 
+    if order_type == "buy":
+        market_buy = shift.Order(shift.Order.Type.MARKET_BUY, symbol, contract_size)
+        trader.submit_order(market_buy)
+
+    if order_type == "sell":
+        market_sell = shift.Order(shift.Order.Type.MARKET_SELL, symbol, contract_size)
+        trader.submit_order(market_sell)
     return
 
 
-if __name__ == "__main__":
-    main(sys.argv)
+def update_data(ls, prices, window=20):
+
+    p=[trader.get_last_price(sym) for sym in ls ]
+    prices.loc[trader.get_last_trade_time(),:] = p
+    # if len(prices)>window:
+    #     prices = prices.iloc[1:,:]
+    return prices
+
+def macd(close,period=(12,26,9)):
+    diff = close.rolling(period[0]).mean()-close.rolling(period[1]).mean()
+    dea = diff.rolling(period[2]).mean()
+    res = diff-dea
+    res.columns = ["macd"]
+    return res
+
+if __name__== "__main__":
+
+    trader = subscribe()
+    ls =  trader.get_stock_list()
+    prices = pd.DataFrame(columns=ls)
+    #ma_table = pd.DataFrame(columns=ls)
+    count = 0
+    count_while = 0
+    while True:
+        print(f"the {count_while} loop")
+        if count_while==0:
+            for _ in range(30):
+                prices = update_data(ls, prices,40)
+                print("pre loop")
+                time.sleep(20)
+        count_while+=1
+        prices = update_data(ls, prices, 40)
+        macd_table = prices.apply(macd,axis=0)
+        latest_diff = macd_table.iloc[-1,:]
+        for symbol,diff in latest_diff.items():
+            if diff >= 0 and trader.get_portfolio_item(symbol).get_long_shares()!=100:
+                size = int((trader.get_portfolio_item(symbol).get_short_shares()+(100-trader.get_portfolio_item(symbol).get_long_shares()))/100)
+                print(f"now buy{symbol}, amount{size}")
+                market_order(trader, "buy", symbol, contract_size=size)
+                count += 1
+                time.sleep(0.5)
+            elif diff < 0 and trader.get_portfolio_item(symbol).get_short_shares()!=100:
+                size = int((trader.get_portfolio_item(symbol).get_long_shares()+(100-trader.get_portfolio_item(symbol).get_short_shares()))/100)
+                print(f"now sell{symbol}, amount{size}")
+                market_order(trader, "sell", symbol, contract_size=size)
+                count += 1
+                time.sleep(0.5)
+            else:
+                pass
+        print(f"prices are: {prices.iloc[-1,:]}")
+        print("="*100)
+        print(f"p&l is {trader.get_portfolio_summary().get_total_realized_pl()}")
+        time.sleep(20)
+        if prices.index[-1].time()>= datetime.time(hour=15,minute=30,second=0):
+            if count<40:
+                for _ in range(40-count):
+                    print(f"buy VIXY")
+                    market_order(trader, "buy", "VIXY", contract_size=5)
+                    time.sleep(90)
+            for item in trader.get_portfolio_items().values():
+                if item.get_shares()<0:
+                    print(f"buy {item.get_symbol()}")
+                    market_order(trader, "buy", item.get_symbol(), contract_size=int(abs(item.get_shares())/100))
+                if item.get_shares()>0:
+                    print(f"sell {item.get_symbol()}")
+                    market_order(trader, "sell", item.get_symbol(), contract_size=int(abs(item.get_shares())/100))
+                time.sleep(1)
+
+
+
+
+
+
+
+
