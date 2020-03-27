2f3793ce46568f96fcd5535f6a48df141a8cb90c
Guanzhuo Qiao <gqiao@stevens.edu>
2020-03-26 23:21:03 -0400

    little modification

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ import shift
 import datetime
 
 def subscribe():
-    trader = shift.Trader("test002")
+    trader = shift.Trader("team-110")
 
     # connect and subscribe to all available order books
     try:
-        trader.connect("initiator.cfg", "password")
+        trader.connect("initiator.cfg", "t7TAFYjhbpN2qSsV")
         trader.sub_all_order_book()
     except shift.IncorrectPasswordError as e:
         print(e)
@@ -39,8 +39,8 @@ def update_data(ls, prices, window=20):
     return prices
 
 def macd(close,period=(12,26,9)):
-    diff = close.rolling(period[0]).mean()-close.rolling(period[1]).mean()
-    dea = diff.rolling(period[2]).mean()
+    diff = close.ewm(span=period[0]).mean()-close.ewm(span=period[1]).mean()
+    dea = diff.ewm(span=period[2]).mean()
     res = diff-dea
     res.columns = ["macd"]
     return res
@@ -59,30 +59,33 @@ if __name__== "__main__":
             for _ in range(30):
                 prices = update_data(ls, prices,40)
                 print("pre loop")
-                time.sleep(20)
+                time.sleep(60)
         count_while+=1
         prices = update_data(ls, prices, 40)
         macd_table = prices.apply(macd,axis=0)
         latest_diff = macd_table.iloc[-1,:]
+        #print(f"diff: {latest_diff}")
         for symbol,diff in latest_diff.items():
-            if diff >= 0 and trader.get_portfolio_item(symbol).get_long_shares()!=100:
+            # print(f"{symbol} and {trader.get_portfolio_item(symbol).get_long_shares()}")
+            # print(f"{symbol} and {trader.get_portfolio_item(symbol).get_short_shares()}")
+            if diff >= 0 and trader.get_portfolio_item(symbol).get_long_shares()<100:
                 size = int((trader.get_portfolio_item(symbol).get_short_shares()+(100-trader.get_portfolio_item(symbol).get_long_shares()))/100)
-                print(f"now buy{symbol}, amount{size}")
+                #print(f"now buy{symbol}, amount{size}")
                 market_order(trader, "buy", symbol, contract_size=size)
                 count += 1
                 time.sleep(0.5)
-            elif diff < 0 and trader.get_portfolio_item(symbol).get_short_shares()!=100:
+            elif diff < 0 and trader.get_portfolio_item(symbol).get_short_shares()<100:
                 size = int((trader.get_portfolio_item(symbol).get_long_shares()+(100-trader.get_portfolio_item(symbol).get_short_shares()))/100)
-                print(f"now sell{symbol}, amount{size}")
+                #print(f"now sell{symbol}, amount{size}")
                 market_order(trader, "sell", symbol, contract_size=size)
                 count += 1
                 time.sleep(0.5)
             else:
                 pass
-        print(f"prices are: {prices.iloc[-1,:]}")
-        print("="*100)
-        print(f"p&l is {trader.get_portfolio_summary().get_total_realized_pl()}")
-        time.sleep(20)
+        # print(f"prices are: {prices.iloc[-1,:]}")
+        # print("="*100)
+        # print(f"p&l is {trader.get_portfolio_summary().get_total_realized_pl()}")
+        time.sleep(60)
         if prices.index[-1].time()>= datetime.time(hour=15,minute=30,second=0):
             if count<40:
                 for _ in range(40-count):
